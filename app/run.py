b8adfe9fd13d69cd599c96250563ade73ff3c1c1
QanaSar <qqanatt@gmail.com>
2024-11-30 05:23:31 +0500

    Изменил порядо роутеров

--- app/run.py
+++ app/run.py
@@ -12,7 +12,7 @@ async def main():
     load_dotenv()
     bot = Bot(token=os.getenv('TOKEN'))
     dp = Dispatcher()
-    dp.include_routers(router, admin)
+    dp.include_routers(admin, router)
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
